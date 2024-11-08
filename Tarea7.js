49cc7408c566487c24310bd231525959799d9ea6
budel0801 <budel0801@gmail.com>
2024-11-08 15:06:37 -0700

    Add files via upload

---
+++ Tarea7.js
@@ -0,0 +1,10 @@
+let nombre ="Baudel Quintana Soto";
+ function
+Contraletras(NombreCompleto) {
+        
+    returnnombreCompleto.replace
+    (/\s+/g,).length;
+ 
+}
+console.log("Número de letras: " + contarLetras(nombre));
+ 
